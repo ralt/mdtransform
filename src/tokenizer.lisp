3392cf7aa63123d3e0d86509f234d843346116d6
Florian Margaine <florian@margaine.com>
2013-05-03 19:49:31 +0200

    Last commit. Markdown is too much of a pain to handle, well not worth the time.

--- src/tokenizer.lisp
+++ src/tokenizer.lisp
@@ -1,9 +1,11 @@
 (in-package #:mdtransform)
 
 (defvar *special-characters* '(("\\\*\\\*" . "\\\*\\\*")
-                              ("\\\*". "\\\*")
+                              ("\\\*" . "\\\*")
                               ("[" . "]")
                               ("![" . "]")))
+
+(defvar *list* '())
 
 (defun tokenize (input)
   "Tokenizes the markdown input"
@@ -17,9 +19,11 @@
 (defun t-word (paragraph)
   "Tokenizes a single paragraph in words"
   (let ((words '())
-        (indexes (get-indexes paragraph)))))
+        (indexes (filter-indexes (get-indexes paragraph))))
+    (format t "~A" indexes)))
 
 (defun get-indexes (paragraph)
+  "Gets the indexes for each special character"
   (let ((ret '()))
     (loop
        for current-char in *special-characters*
@@ -32,11 +36,28 @@
     ret))
 
 (defun collect-indexes (indexes)
+  "Gets the indexes for a special character"
   (loop
      while indexes
      for a = (pop indexes)
      for b = (pop indexes)
      collect (list a b)))
+
+(defun filter-indexes (indexes)
+  (let ((*list* indexes))
+    (remove-if #'filter-index indexes)))
+
+(defun filter-index (index)
+  (format t "~A~%" index)
+  (format t "~A~%" *list*)
+  (let ((found? nil))
+    (loop
+       for i in *list*
+       when (and (> (car index)
+                    (car i))
+                 (< (cdr index)
+                    (cdr i)))
+       do (setf found? t))))
 
 (defun t-paragraphs (text)
   "Tokenizes into paragraphs"
