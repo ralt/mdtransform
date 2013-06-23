7d38deb51c7f131a29e2bfc85f8f7125946f0513
Florian Margaine <florian@margaine.com>
2013-06-23 14:52:45 +0200

    Starting to lex = and -

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -9,6 +9,9 @@
 (defconstant s-in-h2 2)
 (defconstant s-in-h3 3)
 (defconstant s-in-h4 4)
+(defconstant s-one-newline 5)
+(defconstant s-in-h1-second-line 6)
+(defconstant s-in-h2-second-line 7)
 
 (defvar *state* s-start
   "The current state.")
@@ -16,22 +19,31 @@
   "The current token being lexed.")
 
 (deflexer #\# (letter)
-          (when-breaker (eq *state* s-start)
-            (setf *state* s-in-h1))
-          (when-breaker (eq *state* s-in-h1)
-            (setf *state* s-in-h2))
-          (when-breaker (eq *state* s-in-h2)
-            (setf *state* s-in-h3))
-          (when-breaker (eq *state* s-in-h3)
-            (setf *state* s-in-h4))
+  (when-breaker (eq *state* s-start)
+    (setf *state* s-in-h1))
+  (when-breaker (eq *state* s-in-h1)
+    (setf *state* s-in-h2))
+  (when-breaker (eq *state* s-in-h2)
+    (setf *state* s-in-h3))
+  (when-breaker (eq *state* s-in-h3)
+    (setf *state* s-in-h4))
 
-          ;; Default behavior when none of the above applied.
-          (setf *current* (concatenate 'string *current* letter)))
+  ;; Default behavior when none of the above applied.
+  (setf *current* (concatenate 'string *current* letter)))
 
 (deflexer #\= (letter)
-  (format t "~A equal ~%" letter))
+  (when-breaker (or (eq *state* s-one-newline)
+                    (eq *state* s-in-h1-second-line))
+    (setf *state* s-in-h1-second-line))
+
+  (setf *current* (concatenate 'string *current* letter)))
 
 (deflexer #\- (letter)
+  (when-breaker (or (eq *state* s-one-newline)
+                    (eq *state* s-in-h2-second-line))
+    (setf *state* s-in-h2-second-line))
+
+  (setf *current* (concatenate 'string *current* letter)) 
   (format t "~A hyphen ~%" letter))
 
 (deflexer #\newline (letter)
