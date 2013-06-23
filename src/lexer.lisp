60f568c8f00e9859a849904220c3a792152a37c5
Florian Margaine <florian@margaine.com>
2013-06-23 04:01:42 +0200

    Starting lexing.

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -5,12 +5,33 @@
 
 ;;; State constants
 (defconstant s-start 0)
+(defconstant s-in-h1 1)
+(defconstant s-in-h2 2)
+(defconstant s-in-h3 3)
+(defconstant s-in-h4 4)
 
 (defvar *state* s-start
   "The current state.")
+(defvar *current* ""
+  "The current token being lexed.")
 
 (deflexer #\# (letter)
-  (format t "~A sharp ~%" letter))
+          (block lexer
+                 (when (eq *state* s-start)
+                   (setf *state* s-in-h1)
+                   (return-from lexer nil))
+                 (when (eq *state* s-in-h1)
+                   (setf *state* s-in-h2)
+                   (return-from lexer nil))
+                 (when (eq *state* s-in-h2)
+                   (setf *state* s-in-h3)
+                   (return-from lexer nil))
+                 (when (eq *state* s-in-h3)
+                   (setf *state* s-in-h4)
+                   (return-from lexer nil))
+
+                 ;; Default behavior when none of the above applied.
+                 (setf *current* (concatenate 'string *current* letter))))
 
 (deflexer #\= (letter)
   (format t "~A equal ~%" letter))
